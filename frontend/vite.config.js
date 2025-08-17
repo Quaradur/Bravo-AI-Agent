64232298cf072f2850e7add9bba6cbd3d4c5e90e
Quaradur <christianietov06@gmail.com>
2025-08-17 22:09:56 +0200

    Ultime modifiche

---
+++ frontend/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000 // Specifichiamo la porta per il server di sviluppo
+  }
+})
+
+
+// =============== FILE 3: /frontend/tailwind.config.js ===============
+// File di configurazione per Tailwind CSS.
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class',
+}
